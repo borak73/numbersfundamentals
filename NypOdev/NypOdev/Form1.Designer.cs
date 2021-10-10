5b3e074020af73e9479ca2cfa7fd794d1eb473d6
Bora Korpe <borakorpe@outlook.com>
2021-10-10 22:45:59 +0300

    Windows Form Application

---
+++ NypOdev/NypOdev/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace NypOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.teksayilarLV = new System.Windows.Forms.ListView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ciftsayilarLV = new System.Windows.Forms.ListView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.asalsayilarLV = new System.Windows.Forms.ListView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.mukemmelLV = new System.Windows.Forms.ListView();
+            this.calistirBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.basTxtBox = new System.Windows.Forms.MaskedTextBox();
+            this.bitTxtBox = new System.Windows.Forms.MaskedTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(13, 197);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 241);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.teksayilarLV);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 213);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tek Sayılar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // teksayilarLV
+            // 
+            this.teksayilarLV.HideSelection = false;
+            this.teksayilarLV.Location = new System.Drawing.Point(0, 0);
+            this.teksayilarLV.Name = "teksayilarLV";
+            this.teksayilarLV.Size = new System.Drawing.Size(767, 213);
+            this.teksayilarLV.TabIndex = 0;
+            this.teksayilarLV.UseCompatibleStateImageBehavior = false;
+            this.teksayilarLV.View = System.Windows.Forms.View.List;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ciftsayilarLV);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 213);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Çift Sayılar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ciftsayilarLV
+            // 
+            this.ciftsayilarLV.HideSelection = false;
+            this.ciftsayilarLV.Location = new System.Drawing.Point(0, 0);
+            this.ciftsayilarLV.Name = "ciftsayilarLV";
+            this.ciftsayilarLV.Size = new System.Drawing.Size(767, 213);
+            this.ciftsayilarLV.TabIndex = 1;
+            this.ciftsayilarLV.UseCompatibleStateImageBehavior = false;
+            this.ciftsayilarLV.View = System.Windows.Forms.View.List;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.asalsayilarLV);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(767, 213);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Asal Sayılar";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // asalsayilarLV
+            // 
+            this.asalsayilarLV.HideSelection = false;
+            this.asalsayilarLV.Location = new System.Drawing.Point(0, 0);
+            this.asalsayilarLV.Name = "asalsayilarLV";
+            this.asalsayilarLV.Size = new System.Drawing.Size(767, 213);
+            this.asalsayilarLV.TabIndex = 2;
+            this.asalsayilarLV.UseCompatibleStateImageBehavior = false;
+            this.asalsayilarLV.View = System.Windows.Forms.View.List;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.mukemmelLV);
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(767, 213);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Mükkemmel Sayılar";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // mukemmelLV
+            // 
+            this.mukemmelLV.HideSelection = false;
+            this.mukemmelLV.Location = new System.Drawing.Point(0, 0);
+            this.mukemmelLV.Name = "mukemmelLV";
+            this.mukemmelLV.Size = new System.Drawing.Size(767, 213);
+            this.mukemmelLV.TabIndex = 3;
+            this.mukemmelLV.UseCompatibleStateImageBehavior = false;
+            this.mukemmelLV.View = System.Windows.Forms.View.List;
+            // 
+            // calistirBTN
+            // 
+            this.calistirBTN.Location = new System.Drawing.Point(489, 54);
+            this.calistirBTN.Name = "calistirBTN";
+            this.calistirBTN.Size = new System.Drawing.Size(75, 23);
+            this.calistirBTN.TabIndex = 0;
+            this.calistirBTN.Text = "Çalıştır";
+            this.calistirBTN.UseVisualStyleBackColor = true;
+            this.calistirBTN.Click += new System.EventHandler(this.calistirBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.basTxtBox);
+            this.groupBox1.Controls.Add(this.bitTxtBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.calistirBTN);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 135);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Giriş Bilgileri";
+            // 
+            // basTxtBox
+            // 
+            this.basTxtBox.Location = new System.Drawing.Point(116, 55);
+            this.basTxtBox.Mask = "00000";
+            this.basTxtBox.Name = "basTxtBox";
+            this.basTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.basTxtBox.TabIndex = 6;
+            this.basTxtBox.ValidatingType = typeof(int);
+            // 
+            // bitTxtBox
+            // 
+            this.bitTxtBox.Location = new System.Drawing.Point(334, 54);
+            this.bitTxtBox.Mask = "00000";
+            this.bitTxtBox.Name = "bitTxtBox";
+            this.bitTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.bitTxtBox.TabIndex = 5;
+            this.bitTxtBox.ValidatingType = typeof(int);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.notify);
+            this.Shown += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ListView teksayilarLV;
+        private System.Windows.Forms.Button calistirBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView ciftsayilarLV;
+        private System.Windows.Forms.ListView asalsayilarLV;
+        private System.Windows.Forms.MaskedTextBox bitTxtBox;
+        private System.Windows.Forms.MaskedTextBox basTxtBox;
+        private System.Windows.Forms.ListView mukemmelLV;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ NypOdev/NypOdev/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace NypOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EkraniAyarla()
+        {
+            basTxtBox.Text = bitTxtBox.Text = "0";
+            basTxtBox.Focus();
+        }
+        private Boolean kontrolEt(int sayi1, int sayi2)
+        {
+            if (sayi1 < 1 || sayi2 > 10000)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        private Boolean Dogrula()
+        {
+            try
+            {
+                Convert.ToInt32(basTxtBox.Text);
+                Convert.ToInt32(bitTxtBox.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        private Boolean asalMi(int sayi)
+        {
+            int i;
+            for (i = 2; i <= sayi - 1; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    return false;
+                }
+            }
+            if (i == sayi)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        private Boolean mukemmelSayiMi(int sayi)
+        {
+            int toplam = 0, n;
+
+            n = sayi;
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            if (toplam == n)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void calistirBTN_Click(object sender, EventArgs e)
+        {
+            teksayilarLV.Items.Clear();
+            ciftsayilarLV.Items.Clear();
+            asalsayilarLV.Items.Clear();
+            mukemmelLV.Items.Clear();
+            int bas = Convert.ToInt32(basTxtBox.Text), bit = Convert.ToInt32(bitTxtBox.Text);
+
+            if (kontrolEt(bas, bit))
+            {
+                MessageBox.Show("Girdiğiniz Değerleri Kontrol Ediniz");
+                EkraniAyarla();
+            }
+            else
+            {
+                if (Dogrula())
+                {
+                    BigInteger tekToplam = 0, tekCarpim = 1, ciftToplam = 0, ciftCarpim = 1, asalToplam = 0, asalCarpim = 1, mukToplam = 0, mukCarpim = 1;
+
+                    for (int i = bas; i <= bit; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            tekToplam += i;
+                            tekCarpim *= i;
+                            teksayilarLV.Items.Add(i.ToString());
+                        }
+                        else if (i % 2 == 0)
+                        {
+                            ciftToplam += i;
+                            ciftCarpim *= i;
+                            ciftsayilarLV.Items.Add(i.ToString());
+
+                        }
+                        if (asalMi(i))
+                        {
+
+                            asalsayilarLV.Items.Add(i.ToString());
+                            asalToplam += i;
+                            asalCarpim *= i;
+                        }
+                        if (mukemmelSayiMi(i))
+                        {
+                            mukemmelLV.Items.Add(i.ToString());
+                            mukToplam += i;
+                            mukCarpim *= i;
+
+                        }
+
+                    }
+
+                    teksayilarLV.Items.Add("Tek Sayıların Toplamı: " + tekToplam.ToString());
+                    teksayilarLV.Items.Add("Tek Sayıların Çarpımı: " + tekCarpim.ToString());
+                    ciftsayilarLV.Items.Add("Çift Sayıların Toplamı: " + ciftToplam.ToString());
+                    ciftsayilarLV.Items.Add("Çift Sayıların Çarpımı: " + ciftCarpim.ToString());
+                    asalsayilarLV.Items.Add("Asal Sayıların Toplamı: " + asalToplam.ToString());
+                    asalsayilarLV.Items.Add("Asal Sayıların Çarpımı: " + asalCarpim.ToString());
+                    mukemmelLV.Items.Add("Mükemmel Sayıların Toplamı: " + mukToplam.ToString());
+                    mukemmelLV.Items.Add("Mükemmel Sayıların Çarpımı: " + mukCarpim.ToString());
+
+                    EkraniAyarla();
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            EkraniAyarla();
+        }
+
+        private void notify(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipText = "Nesne Yönelimli Programlama";
+            notifyIcon1.BalloonTipTitle = "Odev1";
+            notifyIcon1.ShowBalloonTip(2000);
+
+        }
+
+    }
+
+}
